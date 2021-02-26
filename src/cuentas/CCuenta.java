c3e18e6c33cb1de25eb22a56bd31300f042e86a0
Juan y Moni <Juan y Moni@DESKTOP-HGK76MM>
2021-02-26 19:32:18 +0100

    Actualizo los cambios en el proyecto Deposito. Un saludo.

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -8,7 +8,7 @@ package cuentas;
  */
 public class CCuenta {
 
-      private String nombre;
+    private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
