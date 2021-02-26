cf3825f6add37b3710201fdf8a0bc23349273890
Juan y Moni <Juan y Moni@DESKTOP-HGK76MM>
2021-02-26 18:18:27 +0100

    Actualizo los cambios en el proyecto Deposito. Un saludo.

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void operativa_cuenta(java.lang.Float cuenta) {
         CCuenta cuenta1;
         double saldoActual;
-
+        
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
